615a5e1f23c30cb382948a8a1e7dcab935c6adb4
Kyblicek <1jakubhonzal@gmail.com>
2025-09-03 22:41:50 +0200

    uprava fotky

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://portfolio-t1f.pages.dev/',
   integrations: [tailwind(), react()],
 });
